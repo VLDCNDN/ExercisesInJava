a980bd6c7c0e314ee76d2224f9056a89ce8b478a
vldcndn <vldcndn@gmail.com>
2022-07-02 22:13:44 +0800

    int binary search

--- src/exercises/challenges/BinarySearch.java
+++ src/exercises/challenges/BinarySearch.java
@@ -3,19 +3,32 @@ package exercises.challenges;
 import java.util.Arrays;
 
 public class BinarySearch {
-    public BinarySearch(String[] arr) {
+    public BinarySearch(String[] arr, String target) {
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
 
     }
 
-    public BinarySearch(int[] arr) {
+    public BinarySearch(int[] arr, int target) {
         Arrays.sort(arr);
 
         int start = 0;
         int end = arr.length - 1;
+        int median = Math.round(arr.length / 2);
+        int steps = 0;
 
-        System.out.println(start + ":" + end);
-
+        while(start <= end) {
+            if(arr[median] == target) {
+                System.out.println("Position: " + median);
+                System.out.println("Step takes: " + steps);
+                break;
+            } else if (target > arr[median]) {
+                start = median + 1;
+            } else if (target < arr[median]) {
+                end = median - 1;
+            }
+            median = Math.round((end + start) / 2);
+            steps++;
+        }
     }
 }
--- src/exercises/challenges/Main.java
+++ src/exercises/challenges/Main.java
@@ -3,12 +3,10 @@ package exercises.challenges;
 public class Main {
     public static void main(String[] args) {
         String[] arr = {"Zebera","April", "May", "June", "JUL"};
-        int[] arr2 = {1, 22,33,66,12,33,65};
+        int[] arr2 = {1, 22,33,66,12,44,65};
 
-        BinarySearch searchText = new BinarySearch(arr);
-        BinarySearch searchNum = new BinarySearch(arr2);
-
-
+        BinarySearch searchText = new BinarySearch(arr, "June");
+        BinarySearch searchNum = new BinarySearch(arr2, 1);
 
     }
 }
